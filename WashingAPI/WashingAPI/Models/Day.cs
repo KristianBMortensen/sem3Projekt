e2ac0c0643f174d8f77f1f84fa9a5cc9e14580ee
Rasmus Bagge <rasm75g5@edu.zealand.dk>
2022-05-04 09:31:49 +0200

    Timeslots

---
+++ WashingAPI/WashingAPI/Models/Day.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WashingAPI.Models
+{
+    public class Day
+    {
+        public string Date { get; set; }
+
+        public Dictionary<string, string> Timeslots = new Dictionary<string, string>()
+        {
+            {"7:30-9:00", null },
+            {"9:00-10:30", null },
+            {"10:30-12:00", null },
+            {"12:00-13:30", null },
+            {"13:30-15:00", null },
+            {"15:00-16:30", null },
+            {"16:30-18:00", null },
+            {"18:00-19:30", null },
+            {"19:30-21:00", null },
+        }
+            
+
+    }
+}
