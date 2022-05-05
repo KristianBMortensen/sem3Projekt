d0f9624560d938e30bcec753e9eb99d62dd1a05d
Magnus <mbjtuts@gmail.com>
2022-05-05 22:45:03 +0200

    login linked til Rest

---
+++ WashingAPI/WashingAPI/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WashingAPI.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WashingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private LoginManager _manager;
+
+        public LoginController()
+        {
+            _manager = new();
+        }
+
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _manager.GetAllTokens();
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            string token = _manager.GetToken(id);
+            if (token == null)
+                return StatusCode(204);
+            return Ok(token);
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public bool Post([FromBody] string id)
+        {
+            return _manager.CreateToken(id);
+        }
+
+        // PUT api/<LoginController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<LoginController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ WashingAPI/WashingAPI/Managers/LoginManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WashingAPI.Managers
+{
+    public class LoginManager
+    {
+        private List<string> Tokens;
+
+        public LoginManager()
+        {
+            Tokens = new();
+            Tokens.Add("102474468596296399731");
+        }
+
+        public List<string> GetAllTokens()
+        {
+            return Tokens;
+        }
+
+        public string GetToken(string id)
+        {
+            return Tokens.Find(t => t == id);
+        }
+
+        public bool CreateToken(string id)
+        {
+            if (GetToken(id) == null)
+            {
+                Tokens.Add(id);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
