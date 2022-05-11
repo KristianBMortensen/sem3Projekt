cbd2ca911af43b197f83cb87b7bc22be66f5bcbe
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-05-11 12:54:26 +0200

    se om der er frit

--- Frontend/js/GetDays.js
+++ Frontend/js/GetDays.js
@@ -3,7 +3,8 @@ const URL = "https://localhost:44323/api/Days"
 Vue.createApp({
     data(){
         return {
-            days : null,
+            today: null,
+            maskineStatus: "Ledig",
         }
     },
 
@@ -14,13 +15,85 @@ Vue.createApp({
     methods: {
         async Getall(){
             try{
-                const response = await axios.get(URL)
-                this.days = await response.data
-                console.log(this.days[0])
+                const newUrl = URL+"/"+this.checkDate()
+                console.log(newUrl)
+                const response = await axios.get(URL+"/"+this.checkDate())
+                this.today = await response.data
+                if(!this.today.greenDay){
+                    await this.updateMaskine()
+                }
+                else{
+                    await this.greenDayHandler()
+                }
             }
             catch(e){
                 console.log(e.message)
             }
         },
+
+        checkDate(){
+            const date = new Date()
+            const year = date.getFullYear()
+            let month = date.getMonth()+1
+            let day = date.getDate()
+            if(month <10){
+                month = "0"+month
+            }
+
+            if(day < 10){
+                day = "0"+day
+            }
+            todayDate = "12-"+month+"-"+year
+            console.log(todayDate)
+
+            return todayDate
+        },
+
+        getTime(){
+            const date = new Date()
+            const hour = date.getUTCHours()+2
+            const minutes = date.getUTCMinutes()
+
+            return hour+":"+minutes
+        },
+
+        checkTime(times){
+            const timeArray = times.split("-")
+            if(timeArray[0] < this.getTime() && timeArray[1] > this.getTime()){
+                return true
+            }
+            return false
+        },
+
+        updateMaskine(){
+            for(let i = 0; i < this.today.timeslots.length; i++){
+                if(this.checkTime(this.today.timeslots[i].resTime)){
+                    this.$refs.vaskemaskine.style.backgroundColor = "var(--optaget-maskine)"
+                    this.$refs.maskineStatus.innerHTML = "12:00-13:30"
+                    this.maskineStatus = "Igang"
+                    console.log("fisk")
+                    return
+                }
+            }
+            this.$refs.vaskemaskine.style.backgroundColor = "var(--ledig-maskine)"
+            this.$refs.maskineStatus.innerHTML = ""
+            this.maskineStatus = "Ledig"
+        },
+
+        async greenDayHandler(){
+            const greenUrl = "https://localhost:44323/api/GreenDays"
+            const response = await axios.get(greenUrl)
+            const isGreen = await response.data
+
+            if(isGreen){
+                this.$refs.vaskemaskine.style.backgroundColor = "var(--optaget-maskine)"
+                this.$refs.maskineStatus.innerHTML = "12:00-13:30"
+                this.maskineStatus = "Igang"
+            }else{
+                this.$refs.vaskemaskine.style.backgroundColor = "var(--ledig-maskine)"
+                this.$refs.maskineStatus.innerHTML = ""
+                this.maskineStatus = "Ledig"
+            }
+        }
     }
 }).mount("#app")
