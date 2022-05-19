c69bdd7510a78310ea041f3d73ce9ffa9c42109e
Fhs15 <janus.o@live.dk>
2022-05-19 12:43:49 +0200

    test

---
+++ WashingAPI/WashingAPITests/Managers/LoginRequestManagerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WashingAPI.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WashingAPI.Managers.Tests
+{
+    [TestClass()]
+    public class LoginRequestManagerTests
+    {
+        [TestMethod()]
+        public void LoginRequestManagerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllRequestsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetRequestTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateSignupRequestTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateLoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteRequestTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
