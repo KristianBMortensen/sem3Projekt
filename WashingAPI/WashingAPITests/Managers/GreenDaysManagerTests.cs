9005ae9a9c2483bfa10c03c1639060c7f3a25db9
Fhs15 <janus.o@live.dk>
2022-05-19 12:59:32 +0200

    (⊙_(⊙_⊙)_⊙)

---
+++ WashingAPI/WashingAPITests/Managers/GreenDaysManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WashingAPI.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WashingAPI.Managers.Tests
+{
+    [TestClass()]
+    public class GreenDaysManagerTests
+    {
+        [TestMethod()]
+        public void GetActionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateLastActionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
