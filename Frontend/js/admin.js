38b3da97c54650acc7fb5216db0b18a4458351ac
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-06-22 10:54:44 +0200

    srfjod

--- Frontend/js/admin.js
+++ Frontend/js/admin.js
@@ -1,4 +1,4 @@
-const url = "https://vasklet.azurewebsites.net//api/LoginRequest/"
+const adminUrl = "https://vasklet.azurewebsites.net/api/LoginRequest"
 
 Vue.createApp({
     data(){
@@ -9,13 +9,13 @@ Vue.createApp({
     },
     methods: {
         async loadAll(){
-            const response = await axios.get(URL)
+            const response = await axios.get(adminUrl)
             this.requests = await response.data
             console.log(this.requests)
         },
         async remove(){
             if(this.requestId != null){
-                const response = await axios.delete(URL + this.requestId)
+                const response = await axios.delete(adminUrl+"/" + this.requestId)
                 this.loadAll()
                 this.closeModal()
             }
@@ -24,7 +24,7 @@ Vue.createApp({
         async accept(id){
             if(id != null && id != ""){
 
-                const response = await axios.put(URL + id)
+                const response = await axios.put(adminUrl + id)
                 console.log("noget er null: id " + id)
                 this.loadAll()
             }else{
